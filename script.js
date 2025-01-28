f21739347eebad7c09093cf55bf3eddc72119131
Shayan Ali <syedshayanali1810@gmail.com>
2025-01-28 14:44:06 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var abc = document.querySelector(".btn")
+
+abc.addEventListener("click" , function () {
+    if (true) {
+        alert("email sent sucessfully")
+    } else {
+        alert("email sent unsucessfully")
+    }
+    
+})
